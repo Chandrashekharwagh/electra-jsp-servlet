1df03966a6198787d2ba6f4883e05bb32811c9bd
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-11 12:13:56 +0530

    commits

--- src/main/java/com/electra/web/model/Payment.java
+++ src/main/java/com/electra/web/model/Payment.java
@@ -13,6 +13,6 @@ public class Payment{
     private Long id;
     private Double amount;
     private Date paymentDate;
-    private int customerId;
-    private int orderId;
+    private Long customerId;
+    private Long orderId;
 }
